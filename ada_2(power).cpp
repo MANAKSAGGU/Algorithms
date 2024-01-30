2a0608ee088ec70c6932554facf12b17aaed84ac
MANAKSAGGU <157345772+MANAKSAGGU@users.noreply.github.com>
2024-01-30 22:04:08 +0530

    Add files via upload

---
+++ ada_2(power).cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+#include<chrono>
+#include <cstdlib>
+using namespace std;
+using namespace std::chrono;
+
+int power(int m,int n)
+{
+    if(n==0)
+    {
+        return 1;
+    }
+    else
+    {
+        return(m*power(m,n-1));
+    }
+}
+
+int main()
+{
+    for(int i=1000;i<=10000 ;i=i+1000)
+    {
+    auto start = high_resolution_clock::now();
+    for(int m =1;m<=10;m++)
+    {
+        int p = power(2,i);
+    }
+    auto stop = high_resolution_clock::now();
+    auto duration = (duration_cast<nanoseconds>(stop-start))/10;
+    cout << "inputs \t" << i    << "\t Time taken: " << duration.count() << "nanoseconds" << endl ;
+    }
+
+    return 0;
+}
